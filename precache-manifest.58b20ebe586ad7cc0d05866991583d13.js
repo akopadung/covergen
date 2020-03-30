b2fd0306e79a4da5b53aa09ad9d2bdd7ab630f37
akopadung <akopadung>
2020-03-30 17:33:21 +0700

    Updates

---
+++ precache-manifest.58b20ebe586ad7cc0d05866991583d13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6596bcff0d2e1f50ff263431dd8af3da",
+    "url": "/covergen/index.html"
+  },
+  {
+    "revision": "1c680cb8ae10b51b00cb",
+    "url": "/covergen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d2404518e8127e83c24",
+    "url": "/covergen/static/js/2.0a043398.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covergen/static/js/2.0a043398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c680cb8ae10b51b00cb",
+    "url": "/covergen/static/js/main.7860980a.chunk.js"
+  },
+  {
+    "revision": "cdec01c5d4d6444e6453",
+    "url": "/covergen/static/js/runtime-main.be75eec8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covergen/static/media/logo.5d5d9eef.svg"
+  }
+]);
